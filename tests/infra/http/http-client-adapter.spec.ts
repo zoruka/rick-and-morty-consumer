b9c563e54fdf5847ba04521bd9f6f3fa26bd37f0
Felipe Mendes <zo.fmendes@gmail.com>
2021-09-17 13:46:04 -0300

    :white_check_mark: ensure params add on request

--- tests/infra/http/http-client-adapter.spec.ts
+++ tests/infra/http/http-client-adapter.spec.ts
@@ -35,4 +35,24 @@ describe('HttpClientAdapter', () => {
 		expect(result.statusCode).toEqual(500);
 		expect(result.body).toEqual({ error: 'error' });
 	});
+
+	test('should add params to path', async () => {
+		const sut = makeSut();
+
+		nock(hostMock)
+			.get('/?string_param=any_value&number_param=10')
+			.reply(200, { data: 'any_data' });
+
+		const result = await sut.request({
+			url: hostMock,
+			method: 'get',
+			params: {
+				string_param: 'any_value',
+				number_param: 10,
+			},
+		});
+
+		expect(result.statusCode).toEqual(200);
+		expect(result.body).toEqual({ data: 'any_data' });
+	});
 });

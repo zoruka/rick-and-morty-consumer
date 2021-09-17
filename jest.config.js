a9da549103acb10131b46eebbd227429408db26d
Felipe Mendes <zo.fmendes@gmail.com>
2021-09-17 12:02:48 -0300

    :package: add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	roots: ['<rootDir>/tests'],
+	collectCoverage: true,
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'@/tests/(.*)': '<rootDir>/tests/$1',
+		'@/(.*)': '<rootDir>/src/$1',
+	},
+};

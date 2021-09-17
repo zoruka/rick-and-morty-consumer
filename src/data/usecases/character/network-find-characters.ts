cec2d53b197132771f88a950b02275d7c859870f
Felipe Mendes <zo.fmendes@gmail.com>
2021-09-17 11:07:05 -0300

    :sparkles: add NetworkFindCharacters

---
+++ src/data/usecases/character/network-find-characters.ts
@@ -0,0 +1,24 @@
+import { Http, HttpClient } from '@/data/protocols';
+import { FindCharacters } from '@/domain/usecases';
+
+export class NetworkFindCharacters implements FindCharacters {
+	constructor(
+		private readonly path: string,
+		private readonly httpClient: HttpClient<FindCharacters.Response>
+	) {}
+
+	async find(
+		params: FindCharacters.Params
+	): Promise<FindCharacters.Response> {
+		const requestResponse = await this.httpClient.request({
+			method: 'get',
+			url: this.path,
+			params,
+		});
+
+		if (requestResponse.statusCode !== Http.StatusCode.Ok)
+			throw requestResponse.body;
+
+		return requestResponse.body;
+	}
+}

dad78b743ccaf6ae74d4c59d5472a22f24cbacde
Felipe Mendes <zo.fmendes@gmail.com>
2021-09-17 12:25:35 -0300

    :recycle: add data on IdentifiedError

--- src/domain/errors/identified-error.ts
+++ src/domain/errors/identified-error.ts
@@ -2,7 +2,8 @@ export abstract class IdentifiedError extends Error {
 	constructor(
 		public readonly type: string,
 		public readonly name: string,
-		message: string
+		message: string,
+		public readonly data?: unknown
 	) {
 		super(message);
 	}

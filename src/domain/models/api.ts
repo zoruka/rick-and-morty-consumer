82112b808cec66ddeeb365a6aef058df9a50d9be
Felipe Mendes <zo.fmendes@gmail.com>
2021-09-17 09:22:24 -0300

    :sparkles: add api model

---
+++ src/domain/models/api.ts
@@ -0,0 +1,17 @@
+import { Character, Episode, Location } from '.';
+
+export namespace API {
+	export type Info = {
+		count: number;
+		pages: number;
+		next: string | null;
+		previous: string | null;
+	};
+
+	export type Pagination<
+		T extends Character.Model | Episode.Model | Location.Model
+	> = {
+		info: Info;
+		results: Array<T>;
+	};
+}
